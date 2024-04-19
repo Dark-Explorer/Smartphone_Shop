805202022f8ac19f5a1e0ac8273379ee4d624aa9
Nguyen Huu Duc <diemannschaft2003@gmail.com>
2024-04-19 14:29:39 +0700

    Initial commit

---
+++ src/main/java/com/ecommerce/smartphoneshop/SmartphoneshopApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.smartphoneshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartphoneshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartphoneshopApplication.class, args);
+	}
+
+}
